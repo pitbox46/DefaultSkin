904748c6f5179acff92cf750abe4c1e482eaa2a2
pitbox <migriswo@iu.edu>
2025-03-01 19:43:09 -0500

    Init 2

--- src/main/java/github/pitbox46/defaultskin/mixin/PlayerInfoMixin.java
+++ src/main/java/github/pitbox46/defaultskin/mixin/PlayerInfoMixin.java
@@ -1,8 +1,17 @@
-package github.pitbox46.defaultskin.mixins;
+package github.pitbox46.defaultskin.mixin;
 
-import com.mojang.realmsclient.dto.PlayerInfo;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.resources.DefaultPlayerSkin;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerInfo.class)
 public class PlayerInfoMixin {
+    @Inject(method = "getSkinLocation", at = @At("HEAD"), cancellable = true)
+    private void setSkinDefault(CallbackInfoReturnable<ResourceLocation> cir) {
+        cir.setReturnValue(DefaultPlayerSkin.getDefaultSkin());
+    }
 }
