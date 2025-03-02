80997c58e5d057e44cee12d9da77123276b7795c
pitbox <migriswo@iu.edu>
2025-03-01 19:42:50 -0500

    Initial commit

---
+++ src/main/java/github/pitbox46/defaultskin/mixin/PlayerInfoMixin.java
@@ -0,0 +1,8 @@
+package github.pitbox46.defaultskin.mixins;
+
+import com.mojang.realmsclient.dto.PlayerInfo;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerInfo.class)
+public class PlayerInfoMixin {
+}
